70bc6a6a20cbaaf2d5ae4fd4025b05336e88c75c
LidiaLil <lipkinal@ya.ru>
2025-06-04 23:04:57 +0300

    First

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.getElementById('card-template').content.querySelector('.card');
+const placesContainer = document.querySelector('.places__list'); 
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+};
 
-// @todo: Вывести карточки на страницу
+function addCardToContainer(containerElement, cardElement) {
+    containerElement.append(cardElement);
+}
+
+function createCard(dataObject, deleteCardCallback) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImg = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImg.src = dataObject.link;
+    cardImg.alt = dataObject.name;
+    cardTitle.textContent = dataObject.name;
+
+    cardDeleteButton.addEventListener('click', () => {
+        deleteCardCallback(cardElement);
+    }) 
+   return cardElement; 
+}
+
+ initialCards.forEach((cardObj) => {
+    const cardNode = createCard(cardObj, deleteCard);
+    console.dir(cardNode);
+    addCardToContainer(placesContainer, cardNode);
+ })
+
+
